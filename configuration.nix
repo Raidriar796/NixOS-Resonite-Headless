4a1ca01463ca714b01969d40d1b0b28b62ba7a72
Raidriar796 <145519075+Raidriar796@users.noreply.github.com>
2024-06-29 18:03:18 -0700

    Added shell scripts
    Added shell scripts in the config that setup, update, and run the headless

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,94 @@
 { config, lib, pkgs, ... }:
 
 let
-  userName = gloopie;
+  nixUserName = "gloopie";
+  betaPassword = "Send /headlessCode to the Resonite Bot for the code";
+  steamUsername = "YOURUSERNAME";
+  steamPassword = "YOURPASSWORD";
+  resoConfig = ''{
+    "universeId": null,
+    "tickRate": 60.0,
+    "maxConcurrentAssetTransfers": 4,
+    "usernameOverride": null,
+    "loginCredential": null,
+    "loginPassword": null,
+    "startWorlds": [
+      {
+        "isEnabled": true,
+        "sessionName": null,
+        "customSessionId": null,
+        "description": null,
+        "maxUsers": 16,
+        "accessLevel": "Anyone",
+        "useCustomJoinVerifier": false,
+        "hideFromPublicListing": null,
+        "tags": null,
+        "mobileFriendly": false,
+        "loadWorldURL": null,
+        "loadWorldPresetName": "Grid",
+        "overrideCorrespondingWorldId": null,
+        "forcePort": null,
+        "keepOriginalRoles": false,
+        "defaultUserRoles": null,
+        "roleCloudVariable": null,
+        "allowUserCloudVariable": null,
+        "denyUserCloudVariable": null,
+        "requiredUserJoinCloudVariable": null,
+        "requiredUserJoinCloudVariableDenyMessage": null,
+        "awayKickMinutes": -1.0,
+        "parentSessionIds": null,
+        "autoInviteUsernames": null,
+        "autoInviteMessage": null,
+        "saveAsOwner": null,
+        "autoRecover": true,
+        "idleRestartInterval": -1.0,
+        "forcedRestartInterval": -1.0,
+        "saveOnExit": false,
+        "autosaveInterval": -1.0,
+        "autoSleep": true
+      }
+    ],
+    "dataFolder": null,
+    "cacheFolder": null,
+    "logsFolder": null,
+    "allowedUrlHosts": null,
+    "autoSpawnItems": null
+  }'';
+  
+  SetupHeadless = pkgs.writeShellScriptBin "SetupHeadless" 
+  ''
+    DepotDownloader -app 2519830 -beta headless -betapassword ${betaPassword} -username ${steamUsername} -password ${steamPassword} -dir ~/Resonite/ -validate
+    rm ~/Resonite/Headless/libfreetype6.so
+    ln -s /var/run/current-system/sw/lib/libfreetype.so.6 ~/Resonite/Headless/libfreetype.so.6
+    mkdir ~/Resonite/Headless/Config/
+    echo '${resoConfig}' >| ~/Resonite/Headless/Config/Config.json
+  '';
+
+  CleanSetupHeadless = pkgs.writeShellScriptBin "CleanSetupHeadless" 
+  ''
+    rm -r ~/Resonite
+    DepotDownloader -app 2519830 -beta headless -betapassword ${betaPassword} -username ${steamUsername} -password ${steamPassword} -dir ~/Resonite/ -validate
+    rm ~/Resonite/Headless/libfreetype6.so
+    ln -s /var/run/current-system/sw/lib/libfreetype.so.6 ~/Resonite/Headless/libfreetype.so.6
+    mkdir ~/Resonite/Headless/Config/
+    echo '${resoConfig}' >| ~/Resonite/Headless/Config/Config.json
+  '';
+
+  UpdateHeadless = pkgs.writeShellScriptBin "UpdateHeadless" 
+  '' 
+    DepotDownloader -app 2519830 -beta headless -betapassword ${betaPassword} -username ${steamUsername} -password ${steamPassword} -dir ~/Resonite/ -validate
+  '';
+
+  UpdateConfig = pkgs.writeShellScriptBin "UpdateConfig" 
+  '' 
+    echo '${resoConfig}' >| ~/Resonite/Headless/Config/Config.json
+  '';
+
+  RunHeadless = pkgs.writeShellScriptBin "RunHeadless" 
+  '' 
+    cd ~/Resonite/Headless/
+    mono ./Resonite.exe
+  '';
 in
 {
   imports = 
@@ -39,6 +126,13 @@ in
     # Headless client dependancies
     pkgs.freetype
     pkgs.mono
+
+    # Shell scripts
+    SetupHeadless
+    CleanSetupHeadless
+    UpdateHeadless
+    UpdateConfig
+    RunHeadless
   ];
 
   services.openssh.enable = true;
